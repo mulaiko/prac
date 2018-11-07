7443c06ffd671b1c5bcf483ecdf08a904935a517
Chanda <mulaikopwapwa@gmail.com>
2018-11-07 16:29:24 +0200

    Make after sign out logic private

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-   def after_sign_out_path_for(resource_or_scope)
-  	users_path
+  private
+  def after_sign_out_path_for(resource_or_scope)
+    users_path
   end
 end
