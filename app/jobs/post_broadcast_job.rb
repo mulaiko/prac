be3aea40206c4b7a02c48ff6158a75a94f29588e
Chanda <mulaikopwapwa@gmail.com>
2018-11-07 20:29:17 +0200

    Fix ids of used to broadcast data

--- app/jobs/post_broadcast_job.rb
+++ app/jobs/post_broadcast_job.rb
@@ -2,7 +2,8 @@ class PostBroadcastJob < ApplicationJob
   queue_as :live_posts
   
   def perform(user, post) 
-    user.following_ids.each do |id|
+    users_to_broadcast_to = user.user_followers.ids << user.id
+    users_to_broadcast_to.each do |id|
       ActionCable.server.broadcast "live_posts: #{id}", content: render(post.decorate)
     end
   end 
