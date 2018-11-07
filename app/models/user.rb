7795f8787065e09dc0239ccb3b4e6f24a76a86a3
Chanda <mulaikopwapwa@gmail.com>
2018-11-07 16:37:48 +0200

    Add validation on name to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 	acts_as_followable
 	acts_as_follower
+  validates :name, presence: true
 	has_many :posts
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
