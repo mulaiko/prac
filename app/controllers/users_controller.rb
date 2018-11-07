2f8fb43c84b994282ec22d8fccf0571cfba782fe
Chanda <mulaikopwapwa@gmail.com>
2018-11-07 16:52:48 +0200

    Skip authentication on index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-	before_action :authenticate_user!, only: [:follow_feed]
+	skip_before_action :authenticate_user!, only: [:index]
 	before_action :set_user, only: [:show]
 
   def index
