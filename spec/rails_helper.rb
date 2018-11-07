dd56373b6ff5deb566564b9ebd68b135dd1ee156
Chanda <mulaikopwapwa@gmail.com>
2018-11-07 19:25:31 +0200

    Add database cleaner for tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,6 +40,18 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = false
   config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
   #FactoryBot.definition_file_paths << File.expand_path('../factories', __FILE__)
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
